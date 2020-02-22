baf7384e9bf879dd2c3db6a002c288a6a3f1f7a2
Coryf65 <coryf65@gmail.com>
2020-02-21 20:11:57 -0600

    adding text and clicking a button

--- virtualBrowser.py
+++ virtualBrowser.py
@@ -1,11 +1,22 @@
 # Python and Selenium!
-
 from selenium import webdriver
-
-#driver = webdriver.Chrome()
 
 # specifiying where the driver is located on my pc
 driver = webdriver.Chrome(executable_path='C:/Users/Cory/Documents/Code/Python_Selenium/ChromeDriver/chromedriver.exe')
 
+# using this site for practice
 driver.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
 
+# going to try type and click a button using python
+message_field = driver.find_element_by_xpath('//*[@id="user-message"]')
+
+# adding my text
+message_field.send_keys('Hello World!')
+
+# this is using X-Path, find using dev tools in browser and right click and copy xpath by an element in the DOM
+show_message_button = driver.find_element_by_xpath('//*[@id="get-input"]/button')
+
+# now forcing a click
+show_message_button.click()
+
+# Now handling Input fields
